96cdf85606257a4130d356882a08a5d2ea493013
Heber F Barra <heberferreirabarra@gmail.com>
2024-04-13 13:00:29 -0300

    criei classes para logging

---
+++ src/main/java/org/modelador/logger/FormatadorJavaLogger.java
@@ -0,0 +1,19 @@
+package org.modelador.logger;
+
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class FormatadorJavaLogger extends Formatter {
+
+    @Override
+    public String format(LogRecord record) {
+        return String.format(
+                "%s - [%s] %s::%s - %s",
+                new Date(record.getMillis()),
+                record.getLevel(),
+                record.getSourceClassName(),
+                record.getSourceMethodName(),
+                record.getThrown());
+    }
+}
---
+++ src/main/java/org/modelador/logger/JavaLoggerFiltro.java
@@ -0,0 +1,13 @@
+package org.modelador.logger;
+
+import java.util.logging.Filter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+public class JavaLoggerFiltro implements Filter {
+
+    @Override
+    public boolean isLoggable(LogRecord record) {
+        return record.getLevel() != Level.INFO;
+    }
+}
