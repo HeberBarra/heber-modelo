9d551a55955d05682e3ea437580f740d53ef713a
Heber F Barra <heberferreirabarra@gmail.com>
2024-04-21 16:49:27 -0300

    lógica para garantir criação do arquivos de configurações

--- src/main/java/org/modelador/Principal.java
+++ src/main/java/org/modelador/Principal.java
@@ -1,10 +1,13 @@
 package org.modelador;
 
+import org.modelador.configurador.Configurador;
 import org.modelador.programa.JanelaPrincipal;
 
 public class Principal {
 
     public static void main(String[] args) {
+        Configurador.criarArquivoConfiguracoes();
+
         JanelaPrincipal janelaPrincipal = new JanelaPrincipal(1080, 720);
     }
 }
