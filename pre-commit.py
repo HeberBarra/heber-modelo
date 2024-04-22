0b1bb766dc053cf7dfb6b33b5d9f70c8aef4c4d8
Heber F Barra <heberferreirabarra@gmail.com>
2024-04-22 16:48:03 -0300

    Melhoria e correção do hook de pre-commit

---
+++ pre-commit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+
+comando_git = 'git diff --staged --name-only'
+resultado_comando = subprocess.check_output(comando_git, shell=True).decode('utf-8') 
+arquivos_modificados = resultado_comando.split('\n')
+arquivos_modificados.pop()
+
+if 'windows' in os.name.lower():
+    comando = r'.\gradlew.bat'
+else:
+    comando = './gradlew'
+
+subprocess.run([comando, 'spotlessApply'])
+
+for arquivo in arquivos_modificados:
+    print(arquivo)
+    subprocess.run(['git', 'add', arquivo])
+
+
