2b562a232eb795e806487e665adb9fd6e5df884e
Heber F Barra <heberferreirabarra@gmail.com>
2024-04-30 18:31:12 -0300

    Primeiro commit

---
+++ src/main/java/org/modelador/Principal.java
@@ -0,0 +1,12 @@
+package org.modelador;
+
+import org.modelador.programa.JanelaPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        JanelaPrincipal janelaPrincipal = new JanelaPrincipal(1080, 720);
+        janelaPrincipal.setVisible(true);
+    }
+
+}
