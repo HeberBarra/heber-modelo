0e46aec77eeebe0f9c913aca144eb4bbed7427d0
Heber F Barra <heberferreirabarra@gmail.com>
2024-05-07 22:49:28 -0300

    adição de métodos para salvar os valores das propriedades modificadas

--- src/main/java/org/modelador/base/componente/Componente.java
+++ src/main/java/org/modelador/base/componente/Componente.java
@@ -1,11 +1,59 @@
 package org.modelador.base.componente;
 
-public class Componente implements ConversorXml {
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import javax.swing.JPanel;
+
+public class Componente extends JPanel implements ConversorXml {
 
     protected int largura;
     protected int altura;
     protected int x;
     protected int y;
+
+    public Componente() {
+        super();
+        setVisible(true);
+    }
+
+    @Override
+    public void setSize(int largura, int altura) {
+        super.setSize(largura, altura);
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    @Override
+    public void setSize(Dimension tamanho) {
+        this.setSize(tamanho.width, tamanho.height);
+    }
+
+    @Override
+    public void setLocation(int x, int y) {
+        super.setLocation(x, y);
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void setLocation(Point posicao) {
+        this.setLocation(posicao.x, posicao.y);
+    }
+
+    @Override
+    public void setBounds(int x, int y, int largura, int altura) {
+        super.setBounds(x, y, largura, altura);
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    @Override
+    public void setBounds(Rectangle retangulo) {
+        this.setBounds(retangulo.x, retangulo.y, retangulo.width, retangulo.height);
+    }
 
     public int getLargura() {
         return largura;
