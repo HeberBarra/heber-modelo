74fed4b5f0ef0029d023a5f6443a6c3e8fedf520
Heber F Barra <heberferreirabarra@gmail.com>
2024-05-30 12:44:13 -0300

    adição de flag --version para imprimir a versão atual do programa

--- src/main/java/org/modelador/Principal.java
+++ src/main/java/org/modelador/Principal.java
@@ -6,6 +6,11 @@ import org.modelador.programa.JanelaPrincipal;
 public class Principal {
 
     public static void main(String[] args) {
+        if (args.length != 0 && args[0].equals("--version")) {
+            System.out.println(Principal.class.getPackage().getImplementationVersion());
+            System.exit(0);
+        }
+
         Configurador.recarregarConfiguracoes();
         JanelaPrincipal janelaPrincipal = new JanelaPrincipal(1080, 720);
     }
