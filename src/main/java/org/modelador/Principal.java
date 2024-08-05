a1a540a92146139ef53947a10349c0820bf18031
Heber F Barra <heberferreirabarra@gmail.com>
2024-08-04 22:05:13 -0300

    Configuração inicial do Spring Boot

--- src/main/java/org/modelador/Principal.java
+++ src/main/java/org/modelador/Principal.java
@@ -1,8 +1,14 @@
 package org.modelador;
 
-import org.modelador.configurador.Configurador;
 import org.modelador.programa.JanelaPrincipal;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@SpringBootApplication
 public class Principal {
 
     public static final String NOME_PROGRAMA = "sheepnator";
@@ -14,7 +20,11 @@ public class Principal {
             System.exit(0);
         }
 
-        Configurador.recarregarConfiguracoes();
-        janelaPrincipal = new JanelaPrincipal(1080, 720);
+        SpringApplication.run(Principal.class, args);
+    }
+
+    @RequestMapping({"/", "/index", "/index.html", "/home", "/home.html"})
+    String index(ModelMap modelMap) {
+        return "index";
     }
 }
