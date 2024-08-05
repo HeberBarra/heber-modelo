09586ba04c0ed8aea360f12ae1f1ee615ea2c2ce
Heber F Barra <heberferreirabarra@gmail.com>
2024-08-04 22:01:09 -0300

    Aplicação da formatação padrão do projeto

--- src/main/java/org/modelador/configurador/PastaConfiguracao.java
+++ src/main/java/org/modelador/configurador/PastaConfiguracao.java
@@ -1,13 +1,12 @@
 package org.modelador.configurador;
 
-import org.modelador.Principal;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.modelador.Principal;
 
 public enum PastaConfiguracao {
-
-    MAC(Paths.get("%s/Library/Application Support/%s/".formatted(System.getProperty("user.home"), Principal.NOME_PROGRAMA))),
+    MAC(Paths.get(
+            "%s/Library/Application Support/%s/".formatted(System.getProperty("user.home"), Principal.NOME_PROGRAMA))),
     UNIX(Paths.get("%s/.config/%s".formatted(System.getProperty("user.home"), Principal.NOME_PROGRAMA))),
     WINDOWS(Paths.get("%s/%s".formatted(System.getenv("APPDATA"), Principal.NOME_PROGRAMA)));
 
