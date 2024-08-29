33f5b7ea734fa6a5d0ab29ada3f627087d1409ca
Heber F Barra <heberferreirabarra@gmail.com>
2024-08-28 21:57:10 -0300

    update no pre commit git hook

--- pre-commit.py
+++ pre-commit.py
@@ -14,13 +14,13 @@ def formatar_arquivos():
 
 
 comando_git = 'git diff --staged --name-only'
-resultado_comando = subprocess.check_output(comando_git, shell=True).decode('utf-8') 
+resultado_comando = subprocess.check_output(comando_git, shell=True)
+resultado_comando = resultado_comando.decode('utf-8')
 arquivos_modificados = resultado_comando.split('\n')
 arquivos_modificados.pop()
 
-if "java" in arquivos_modificados:
+if any('java' in arquivo for arquivo in arquivos_modificados):
     formatar_arquivos()
 
 for arquivo in arquivos_modificados:
     subprocess.run(['git', 'add', arquivo])
-
