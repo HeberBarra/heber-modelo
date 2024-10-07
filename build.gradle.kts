30ef5f25722f15543a9c8081c867f240aa74dbc5
Heber F Barra <heberferreirabarra@gmail.com>
2024-10-06 22:28:12 -0300

    Atualização para JDK 23

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
 }
 
 dependencies {
