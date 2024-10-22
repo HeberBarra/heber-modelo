65ce46cfd8864548815b23360e488c1b70c50efa
Heber F Barra <heberferreirabarra@gmail.com>
2024-10-22 09:58:49 -0300

    Adição de testes para a classe CalculadorHash

---
+++ src/main/java/org/modelador/calculadorhash/EntradaCalculador.java
@@ -0,0 +1,11 @@
+package org.modelador.calculadorhash;
+
+import java.nio.charset.StandardCharsets;
+
+public record EntradaCalculador(String entrada, String saida) {
+
+    public byte[] getEntradaBytes() {
+        return entrada.getBytes(StandardCharsets.UTF_8);
+    }
+
+}
