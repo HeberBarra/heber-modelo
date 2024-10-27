ae3bb46c4d6f4e17931ee700c75d74dcc0615f44
Heber F Barra <heberferreirabarra@gmail.com>
2024-10-27 12:10:44 -0300

    Adicionei doc comments na classe EjetorArquivosBanco

--- src/main/java/org/modelador/banco/EjetorArquivosBanco.java
+++ src/main/java/org/modelador/banco/EjetorArquivosBanco.java
@@ -12,6 +12,12 @@ import org.modelador.configurador.Configurador;
 import org.modelador.configurador.Recurso;
 import org.modelador.logger.JavaLogger;
 
+/**
+ * Faz uma cópia de certos arquivos que estão dentro do JAR e joga eles para fora, permitindo assim que o usuário
+ * possa utilizá-los mais facilmente.
+ * <p>
+ * Considere "ejetar" nesse contexto, como fazer uma cópia de um arquivo.
+ * */
 public class EjetorArquivosBanco {
 
     private static final Logger logger = JavaLogger.obterLogger(EjetorArquivosBanco.class.getName());
@@ -21,19 +27,27 @@ public class EjetorArquivosBanco {
     public static final String CONFIGURAR_USERS = PASTA_SCRIPTS + "02 - CriarUsuarios.sh";
     public static final String ARQUIVO_DOCKER_COMPOSE = PASTA_ARQUIVOS_BANCO + "docker-compose.yml";
     private final Configurador configurador;
-    private String destinoArquivos;
+    private final String destinoArquivos;
 
     public EjetorArquivosBanco() {
         this.configurador = Configurador.getInstance();
         this.destinoArquivos = configurador.pegarValorConfiguracao("ejetor", "destino", String.class);
     }
 
+    /**
+     * Ejeta os arquivos utilizados para configurar a base de dados
+     * */
     public void ejetarScriptsConfiguracao() {
         String pastaDestino = destinoArquivos + "init/";
         ejetarArquivo(pastaDestino, CONFIGURAR_BANCO);
         ejetarArquivo(pastaDestino, CONFIGURAR_USERS);
     }
 
+    /**
+     * Ejeta o arquivo utilizado para criar o docker compose do programa, opcionalmente, caso o usuário deseje
+     * (decido pela configuração do programa), cria uma cópia do arquivo env para o mesmo local para onde será
+     * ejetado o arquivo docker compose
+     * */
     public void ejetarDockerCompose() {
         ejetarArquivo(destinoArquivos, ARQUIVO_DOCKER_COMPOSE);
 
@@ -42,6 +56,14 @@ public class EjetorArquivosBanco {
         }
     }
 
+    /**
+     * Ejeta um arquivo específico de dentro do JAR, para o destino desejado, copiando o conteúdo do arquivo original
+     * para o gerado
+     * <p>
+     * Cria a pasta de destino e suas antecessoras, caso as mesmas não existam
+     * @param pastaDestino a pasta na qual o arquivo deve ser salvo
+     * @param arquivo o arquivo a ser ejetado pelo programa
+     */
     private void ejetarArquivo(String pastaDestino, String arquivo) {
         try (InputStream arquivoExtraido = Recurso.pegarRecurso(arquivo)) {
             String nomeArquivo = Arrays.stream(arquivo.split("/")).toList().getLast();
@@ -57,6 +79,9 @@ public class EjetorArquivosBanco {
         }
     }
 
+    /**
+     * Criar um link simbólico para o arquivo env especificado nas configurações do programa, permitindo assim passar
+     * segredos para os arquivos ejetados mais facilmente.*/
     @SuppressWarnings("ResultOfMethodCallIgnored")
     private void copiarArquivoDotEnv() {
         String nomeEnv = configurador.pegarValorConfiguracao("ejetor", "nome_arquivo_env", String.class);
@@ -76,11 +101,4 @@ public class EjetorArquivosBanco {
         }
     }
 
-    public String getDestinoArquivos() {
-        return destinoArquivos;
-    }
-
-    public void setDestinoArquivos(String destinoArquivos) {
-        this.destinoArquivos = destinoArquivos;
-    }
 }
