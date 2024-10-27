d815a023f4af0629b8cc0c535f7f06fc485a9969
Heber F Barra <heberferreirabarra@gmail.com>
2024-10-27 11:50:12 -0300

    Adicionei doc comments na classe CalculadorHash

--- src/main/java/org/modelador/calculadorhash/CalculadorHash.java
+++ src/main/java/org/modelador/calculadorhash/CalculadorHash.java
@@ -6,12 +6,21 @@ import java.util.logging.Logger;
 import org.modelador.codigosaida.CodigoSaida;
 import org.modelador.logger.JavaLogger;
 
-// https://www.baeldung.com/sha-256-hashing-java
-
+/**
+ * Baseado em: <a href="https://www.baeldung.com/sha-256-hashing-java">SHA-256 Java</a>
+ * <p>
+ * Utilizado para calcular o hash utilizando vários algoritmos, veja os métodos disponíveis para descobrir os algoritmos disponíveis
+ * */
 public class CalculadorHash {
 
     private static final Logger logger = JavaLogger.obterLogger(CalculadorHash.class.getName());
 
+    /**
+     * Calcula o hash de um array dados utilizando um algoritmo específico
+     * @param messageDigest especifica o algoritmo que será utilizado para o cálculo do hash
+     * @param dados os dados a serem processados pelo algoritmo
+     * @return a string do hash calculado
+     * */
     private String calcularHash(MessageDigest messageDigest, byte[] dados) {
         byte[] mensagemHex = messageDigest.digest(dados);
         StringBuilder hexString = new StringBuilder(mensagemHex.length * 2);
@@ -29,6 +38,11 @@ public class CalculadorHash {
         return hexString.toString();
     }
 
+    /**
+     * Calcula o hash sum de um dado array de bytes utilizando o algoritmo SHA-256
+     * @param dados os dados que serão processados
+     * @return a string do hash calculado
+     * */
     public String calcularHash256(byte[] dados) {
         MessageDigest messageDigest;
         try {
@@ -43,6 +57,11 @@ public class CalculadorHash {
         return calcularHash(messageDigest, dados);
     }
 
+    /**
+     * Calcula o hash sum de um dado array de bytes utilizando o algoritmo SHA-512
+     * @param dados os dados que serão processados
+     * @return a string do hash calculado
+     * */
     public String calcularHash512(byte[] dados) {
         MessageDigest messageDigest;
         try {
