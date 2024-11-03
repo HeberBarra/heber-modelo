f26842885c7292ae10ac778adf8da9f57703a475
Heber F Barra <heberferreirabarra@gmail.com>
2024-11-03 12:55:59 -0300

    Crei uma classe DAO genérica e classe base para as entidades do banco

---
+++ src/main/java/org/modelador/banco/GenericDAO.java
@@ -0,0 +1,54 @@
+package org.modelador.banco;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import java.util.List;
+
+/**
+ * Classe base de todos os DAOs do programa.
+ * @since v0.0.6-SNAPSHOT
+ * @see Entidade
+ * */
+public abstract class GenericDAO<E extends Entidade> {
+
+    protected Class<E> classeEntidade;
+    protected EntityManager entityManager;
+
+    public GenericDAO(Class<E> classeEntidade) {
+        this.classeEntidade = classeEntidade;
+        this.entityManager = ConectorBanco.getInstance().criarEntityManager();
+    }
+
+    public void criar(E entidade) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(entidade);
+        entityManager.getTransaction().commit();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<E> list() {
+        String hqlList = "from %s".formatted(classeEntidade.getSimpleName());
+        Query hqlQuery = entityManager.createQuery(hqlList);
+        return (List<E>) hqlQuery.getResultList();
+    }
+
+    public E findByID(int id) {
+        return entityManager.find(classeEntidade, id);
+    }
+
+    public void update(E entidade) {
+        entityManager.getTransaction().begin();
+        entityManager.merge(entidade);
+        entityManager.getTransaction().commit();
+    }
+
+    public void delete(E entidade) {
+        entityManager.getTransaction().begin();
+        entityManager.remove(entidade);
+        entityManager.getTransaction().commit();
+    }
+
+    public void fecharConexao() {
+        entityManager.close();
+    }
+}
