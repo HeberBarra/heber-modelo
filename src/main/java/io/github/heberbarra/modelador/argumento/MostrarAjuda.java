d555ed37dd7865541a8fb7c05540ed367bf9cf5a
Heber F Barra <heberferreirabarra@gmail.com>
2024-12-27 09:05:13 -0300

    Argumento para mostrar os arugmentos disponíveis

---
+++ src/main/java/io/github/heberbarra/modelador/argumento/MostrarAjuda.java
@@ -0,0 +1,55 @@
+package io.github.heberbarra.modelador.argumento;
+
+import io.github.heberbarra.modelador.argumento.coletor.ColetorClassesArgumentos;
+import io.github.heberbarra.modelador.codigosaida.CodigoSaida;
+import io.github.heberbarra.modelador.logger.JavaLogger;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+import org.fusesource.jansi.Ansi;
+
+public class MostrarAjuda extends Argumento {
+
+    private static final Logger logger = JavaLogger.obterLogger(MostrarAjuda.class.getName());
+    private final ColetorClassesArgumentos coletorClassesArgumentos;
+
+    public MostrarAjuda() {
+        coletorClassesArgumentos = ColetorClassesArgumentos.getInstance();
+        this.descricao = "Mostra os comandos/flags disponíveis e as suas descrições.";
+        this.flagsPermitidas = List.of("--ajuda", "--help", "-h");
+    }
+
+    @Override
+    public void run() {
+        Set<Class<Argumento>> argumentos = coletorClassesArgumentos.getArgumentos();
+
+        for (Class<Argumento> classeArgumento : argumentos) {
+            try {
+                Constructor<Argumento> construtorArgumento = classeArgumento.getConstructor();
+                Argumento argumento = construtorArgumento.newInstance();
+                imprimirInfo(argumento);
+            } catch (NoSuchMethodException e) {
+                logger.warning("Não foi possível pegar o construtor da classe %s. Erro: %s"
+                        .formatted(classeArgumento.getName(), e.getMessage()));
+                System.exit(CodigoSaida.ERRO_PEGAR_CONSTRUTOR.getCodigo());
+            } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                logger.warning("Não foi possível criar o objeto para a classe %s. Erro: %s"
+                        .formatted(classeArgumento.getName(), e.getMessage()));
+                System.exit(CodigoSaida.ERRO_CRIACAO_OBJETO.getCodigo());
+            }
+        }
+
+        System.exit(CodigoSaida.OK.getCodigo());
+    }
+
+    private void imprimirInfo(Argumento argumento) {
+        String nomeArgumento = argumento.getClass().getSimpleName();
+        System.out.println(
+                Ansi.ansi().a("[").fg(Ansi.Color.CYAN).a(nomeArgumento).reset().a("]"));
+        System.out.println(String.join(" ", argumento.getFlagsPermitidas()));
+        System.out.println(argumento.getDescricao());
+        System.out.println();
+    }
+}
