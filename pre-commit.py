26ef8bf34e3cd09807809ddd60000fea39e3a444
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-04 11:14:24 -0300

    [ HOOK ] Melhoria no hook de pre-commit para formatar arquivos HTML, SCSS e Typescript

--- pre-commit.py
+++ pre-commit.py
@@ -4,13 +4,23 @@ import subprocess
 import os
 
 
-def formatar_arquivos():
+def formatar_java():
     if 'windows' in os.name.lower():
         comando = r'.\gradlew.bat'
     else:
         comando = './gradlew'
 
     subprocess.run([comando, 'spotlessApply'])
+
+
+def formatar_prettier():
+    comando_formatar_html = ['npx', 'prettier', '--write', 'src/main/resources/templates']
+    comando_formatar_scss = ['npx', 'prettier', '--write', 'src/main/resources/static/scss']
+    comando_formatar_type = ['npx', 'prettier', '--write', 'src/main/resources/static/typescript']
+
+    subprocess.run(comando_formatar_html)
+    subprocess.run(comando_formatar_scss)
+    subprocess.run(comando_formatar_type)
 
 
 comando_git = 'git diff --staged --name-only'
@@ -20,7 +30,10 @@ arquivos_modificados = resultado_comando.split('\n')
 arquivos_modificados.pop()
 
 if any('java' in arquivo for arquivo in arquivos_modificados):
-    formatar_arquivos()
+    formatar_java()
+
+if any(tipo_arquivo for tipo_arquivo in ['html', 'scss', 'ts'] if any(tipo_arquivo in arquivo for arquivo in arquivos_modificados)):
+    formatar_prettier()
 
 for arquivo in arquivos_modificados:
     subprocess.run(['git', 'add', arquivo])
