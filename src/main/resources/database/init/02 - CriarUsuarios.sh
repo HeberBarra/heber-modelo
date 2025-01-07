06b21d9bddbbc165237c601466edbd2a2d727a08
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-07 17:14:36 -0300

    [ REFACTOR ] Mudança na criação dos usuários para permitir login de qualquer host

--- src/main/resources/database/init/02 - CriarUsuarios.sh
+++ src/main/resources/database/init/02 - CriarUsuarios.sh
@@ -2,12 +2,21 @@
 set -e
 
 mysql --protocol=socket -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOSQL
-CREATE USER IF NOT EXISTS 'estudante'@'$MYSQL_HOST' IDENTIFIED BY '$SENHA_ESTUDANTE';
-GRANT INSERT, SELECT ON db_HeberModelo.tbAtividade TO 'estudante'@'$MYSQL_HOST';
-GRANT INSERT, SELECT ON db_HeberModelo.tbUsuario TO 'estudante'@'$MYSQL_HOST';
-GRANT SELECT ON db_HeberModelo.tbFeedback TO 'estudante'@'$MYSQL_HOST';
+CREATE USER IF NOT EXISTS 'estudante'@'%' IDENTIFIED BY '$SENHA_ESTUDANTE';
+GRANT INSERT, SELECT ON db_HeberModelo.tbAtividade TO 'estudante'@'%';
+GRANT INSERT, SELECT ON db_HeberModelo.tbUsuario TO 'estudante'@'%';
+GRANT SELECT ON db_HeberModelo.tbFeedback TO 'estudante'@'%';
 
-CREATE USER IF NOT EXISTS 'professor'@'$MYSQL_HOST' IDENTIFIED BY '$SENHA_PROFESSOR';
-GRANT INSERT, SELECT, UPDATE, DELETE ON db_HeberModelo.* TO 'professor'@'$MYSQL_HOST';
+CREATE USER IF NOT EXISTS 'estudante'@localhost IDENTIFIED BY '$SENHA_ESTUDANTE';
+GRANT INSERT, SELECT ON db_HeberModelo.tbAtividade TO 'estudante'@localhost;
+GRANT INSERT, SELECT ON db_HeberModelo.tbUsuario TO 'estudante'@localhost;
+GRANT SELECT ON db_HeberModelo.tbFeedback TO 'estudante'@localhost;
+
+CREATE USER IF NOT EXISTS 'professor'@'%' IDENTIFIED BY '$SENHA_PROFESSOR';
+GRANT INSERT, SELECT, UPDATE, DELETE ON db_HeberModelo.* TO 'professor'@'%';
+
+CREATE USER IF NOT EXISTS 'professor'@localhost IDENTIFIED BY '$SENHA_PROFESSOR';
+GRANT INSERT, SELECT, UPDATE, DELETE ON db_HeberModelo.* TO 'professor'@localhost;
+
 FLUSH PRIVILEGES;
 EOSQL
