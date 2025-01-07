78175d0f00ead9b9217bb848d03871c4f926abc1
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-07 17:19:45 -0300

    [ FEATURE ] Ajuste no ejetor para ejetar o arquivo Dockerfile

--- src/main/java/io/github/heberbarra/modelador/argumento/EjetorArquivosDocker.java
+++ src/main/java/io/github/heberbarra/modelador/argumento/EjetorArquivosDocker.java
@@ -6,13 +6,14 @@ import io.github.heberbarra.modelador.configurador.ConfiguradorPrograma;
 import java.util.List;
 
 /**
- * Ejeta o arquivo docker compose
+ * Ejeta o arquivo docker compose o arquivo Dockerfile
+ *
  * @since v0.0.4-SNAPSHOT
- * */
+ */
 public class EjetorArquivosDocker extends Argumento {
 
     public EjetorArquivosDocker() {
-        this.descricao = "Ejeta o arquivo docker-compose.yml de dentro do jar. "
+        this.descricao = "Ejeta o arquivo docker-compose.yml e o arquivo Dockerfile de dentro do jar. "
                 + "Obs.: caso não existam, os arquivos de configuração serão gerados";
         this.flagsPermitidas = List.of("--eject-docker-compose", "--ejetar-docker-compose");
     }
@@ -20,7 +21,7 @@ public class EjetorArquivosDocker extends Argumento {
     /**
      * Ejeta o arquivo docker compose, mas primeiro gera os arquivos de configuração caso não existam,
      * pois é necessário saber para onde copiar o arquivo. Encerra o programa após ejetar o arquivo.
-     * */
+     */
     @Override
     public void run() {
         ConfiguradorPrograma configurador = ConfiguradorPrograma.getInstance();
