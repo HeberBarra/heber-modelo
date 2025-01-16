e6088afef3ba7b33f32af1fecca7d7fe5e98b4ba
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-15 22:15:23 -0300

    [ ENHANCEMENT ] Adição de avisos de copyright nos arquivos TS e SCSS

--- src/main/resources/static/typescript/avisoCookies.ts
+++ src/main/resources/static/typescript/avisoCookies.ts
@@ -1,3 +1,15 @@
+/**
+ * Copyright (C) 2025 Heber Ferreira Barra, João Gabriel de Cristo, Matheus Jun Alves Matuda.
+ *
+ * Licensed under the Massachusetts Institute of Technology (MIT) License.
+ * You may obtain a copy of the license at:
+ *
+ *     https://choosealicense.com/licenses/mit/
+ *
+ * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ */
+
 let avisoCookies: HTMLDivElement | null = document.querySelector("#aviso-cookies");
 let btnAviso: HTMLButtonElement | null = document.querySelector("#aviso-cookies button");
 
--- src/main/resources/static/typescript/desligar.ts
+++ src/main/resources/static/typescript/desligar.ts
@@ -1,3 +1,15 @@
+/**
+ * Copyright (C) 2025 Heber Ferreira Barra, João Gabriel de Cristo, Matheus Jun Alves Matuda.
+ *
+ * Licensed under the Massachusetts Institute of Technology (MIT) License.
+ * You may obtain a copy of the license at:
+ *
+ *     https://choosealicense.com/licenses/mit/
+ *
+ * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ */
+
 let btnDesligar: HTMLButtonElement | null = document.querySelector("#btn-desligar");
 let formDesligar: HTMLFormElement | null = document.querySelector("#form-desligar");
 
