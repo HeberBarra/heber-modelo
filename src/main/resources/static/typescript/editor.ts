387ee6aa086b1f436db8f8d561a6fa9513919d9a
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-21 17:12:42 -0300

    [ FEATURE ] Lógica para detectar se a borda do componente foi pressionada

--- src/main/resources/static/typescript/editor.ts
+++ src/main/resources/static/typescript/editor.ts
@@ -6,6 +6,9 @@ let offsetY: number;
 componentes.forEach((componente) => {
   componente.addEventListener("mousedown", (e) => {
     e.preventDefault();
+    if (verificarDentroBorda(componente, e.clientX, e.clientY)) {
+      return;
+    }
     offsetX = e.clientX - componente.getBoundingClientRect().left;
     offsetY = e.clientY - componente.getBoundingClientRect().top;
     componente.classList.add("dragging");
@@ -19,12 +22,28 @@ componentes.forEach((componente) => {
   });
 });
 
+const verificarDentroBorda = (componente: HTMLElement, x: number, y: number): boolean => {
+  let estiloComponente = getComputedStyle(componente);
+  let larguraBorda: number = converterTamanhoParaNumero(estiloComponente.borderWidth);
+  let alturaElemento: number = converterTamanhoParaNumero(estiloComponente.height);
+  let larguraElemento: number = converterTamanhoParaNumero(estiloComponente.width);
+  let posicaoX = x - componente.getBoundingClientRect().left;
+  let posicaoY = y - componente.getBoundingClientRect().top;
+
+  return (
+    (posicaoX <= larguraBorda || posicaoX >= larguraElemento - larguraBorda) &&
+    (posicaoY <= larguraBorda || posicaoY >= alturaElemento - larguraElemento)
+  );
+};
+
+const converterTamanhoParaNumero = (tamanho: string): number => {
+  return Number(tamanho.substring(0, tamanho.length - 2));
+};
+
 const dragElement = (event: MouseEvent) => {
   event.preventDefault();
   let x = event.clientX - offsetX;
   let y = event.clientY - offsetY;
-  console.log(x);
-  console.log(y);
   componenteAtual.style.left = `${x}px`;
   componenteAtual.style.top = `${y}px`;
 };
