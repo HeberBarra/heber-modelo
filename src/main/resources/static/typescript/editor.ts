c2f12f4c561ae5899bb50e311cd25ca80f00a6ff
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-27 22:31:13 -0300

    [ FEATURE ] Estilização inicial dos paíneis laterais da página de edição

--- src/main/resources/static/typescript/editor.ts
+++ src/main/resources/static/typescript/editor.ts
@@ -1,18 +1,56 @@
+// Trocar página painel lateral
+let paginasPainelDireito: NodeListOf<HTMLElement> =
+  document.querySelectorAll("#painel-direito .pagina");
+let paginasPainelEsquerdo: NodeListOf<HTMLElement> = document.querySelectorAll(
+  "#painel-esquerdo .pagina",
+);
+
+// Esconder painel
+let painelDireito: HTMLElement | null = document.querySelector("#painel-direito");
+let painelEsquerdo: HTMLElement | null = document.querySelector("#painel-esquerdo");
+let btnPainelDireito: HTMLButtonElement | null = document.querySelector(
+  "#painel-direito button.alternar-painel",
+);
+
+let btnPainelEsquerdo: HTMLButtonElement | null = document.querySelector(
+  "#painel-esquerdo button.alternar-painel",
+);
+
+btnPainelDireito?.addEventListener("click", (e) => {
+  return alternarTextoBotao(e.target as HTMLButtonElement | null);
+});
+
+btnPainelDireito?.addEventListener("click", () => alternarPainel(painelDireito));
+
+btnPainelEsquerdo?.addEventListener("click", (e) => {
+  return alternarTextoBotao(e.target as HTMLButtonElement | null);
+});
+
+btnPainelEsquerdo?.addEventListener("click", () => alternarPainel(painelEsquerdo));
+
+const alternarTextoBotao = (btnAlvo: HTMLButtonElement | null) => {
+  if (btnAlvo == null) return;
+
+  if (btnAlvo.innerHTML === "&lt;") {
+    btnAlvo.innerHTML = "&gt;";
+  } else {
+    btnAlvo.innerHTML = "&lt;";
+  }
+};
+
+const alternarPainel = (painelAlvo: HTMLElement | null) => {
+  if (painelAlvo == null) return;
+};
+
+// Movimentação dos componentes
 let componentes: NodeListOf<HTMLDivElement> = document.querySelectorAll(".componente");
 let componenteAtual: HTMLDivElement;
 let offsetX: number;
 let offsetY: number;
-let xInicial: number;
-let redimensionando: boolean = false;
 
 componentes.forEach((componente) => {
   componente.addEventListener("mousedown", (e) => {
     e.preventDefault();
-    if (verificarDentroBorda(componente, e.clientX, e.clientY)) {
-      redimensionando = true;
-      xInicial = e.clientX;
-      return;
-    }
     offsetX = e.clientX - componente.getBoundingClientRect().left;
     offsetY = e.clientY - componente.getBoundingClientRect().top;
     componente.classList.add("dragging");
@@ -20,40 +58,11 @@ componentes.forEach((componente) => {
     componenteAtual = componente;
   });
 
-  componente.addEventListener("mousemove", (e) => {
-    if (!redimensionando) return;
-
-    componente.style.width = `${componente.getBoundingClientRect().width + (e.clientX - xInicial)}px`;
-    xInicial = e.clientX;
-  });
-
   componente.addEventListener("mouseup", () => {
     componente.classList.remove("dragging");
     document.removeEventListener("mousemove", dragElement);
   });
 });
-
-document.addEventListener("mouseup", () => {
-  redimensionando = false;
-});
-
-const verificarDentroBorda = (componente: HTMLElement, x: number, y: number): boolean => {
-  let estiloComponente = getComputedStyle(componente);
-  let larguraBorda: number = converterTamanhoParaNumero(estiloComponente.borderWidth);
-  let alturaElemento: number = converterTamanhoParaNumero(estiloComponente.height);
-  let larguraElemento: number = converterTamanhoParaNumero(estiloComponente.width);
-  let posicaoX = x - componente.getBoundingClientRect().left;
-  let posicaoY = y - componente.getBoundingClientRect().top;
-
-  return (
-    (posicaoX <= larguraBorda || posicaoX >= larguraElemento - larguraBorda) &&
-    (posicaoY <= larguraBorda || posicaoY >= alturaElemento - larguraElemento)
-  );
-};
-
-const converterTamanhoParaNumero = (tamanho: string): number => {
-  return Number(tamanho.substring(0, tamanho.length - 2));
-};
 
 const dragElement = (event: MouseEvent) => {
   event.preventDefault();
