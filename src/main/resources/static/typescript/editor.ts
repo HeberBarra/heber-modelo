8d4e4d9d9f02c072243e6b7369f1c56330d21825
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-29 08:44:13 -0300

    [ FEATURE ] Funcionalidade para esconder(ainda sem reverter) os paíneis laterais

--- src/main/resources/static/typescript/editor.ts
+++ src/main/resources/static/typescript/editor.ts
@@ -1,3 +1,7 @@
+const converterPXParaNumero = (tamanho: string): number => {
+  return Number(tamanho.substring(0, tamanho.length - 2));
+};
+
 // Trocar página painel lateral
 let paginasPainelDireito: NodeListOf<HTMLElement> =
   document.querySelectorAll("#painel-direito .pagina");
@@ -20,13 +24,17 @@ btnPainelDireito?.addEventListener("click", (e) => {
   return alternarTextoBotao(e.target as HTMLButtonElement | null);
 });
 
-btnPainelDireito?.addEventListener("click", () => alternarPainel(painelDireito));
+btnPainelDireito?.addEventListener("click", () => {
+  alternarPainel(painelDireito, btnPainelDireito);
+});
 
 btnPainelEsquerdo?.addEventListener("click", (e) => {
   return alternarTextoBotao(e.target as HTMLButtonElement | null);
 });
 
-btnPainelEsquerdo?.addEventListener("click", () => alternarPainel(painelEsquerdo));
+btnPainelEsquerdo?.addEventListener("click", () =>
+  alternarPainel(painelEsquerdo, btnPainelEsquerdo),
+);
 
 const alternarTextoBotao = (btnAlvo: HTMLButtonElement | null) => {
   if (btnAlvo == null) return;
@@ -38,8 +46,76 @@ const alternarTextoBotao = (btnAlvo: HTMLButtonElement | null) => {
   }
 };
 
-const alternarPainel = (painelAlvo: HTMLElement | null) => {
-  if (painelAlvo == null) return;
+const alternarPainel = (painelAlvo: HTMLElement | null, btnPainel: HTMLButtonElement | null) => {
+  if (painelAlvo == null || btnPainel == null) return;
+
+  if (painelAlvo.classList.contains(".hidden")) {
+    painelAlvo.classList.remove(".hidden");
+    painelAlvo.style.width = "auto";
+    painelAlvo.style.height = "auto";
+    painelAlvo.style.removeProperty("border");
+    btnPainel.style.removeProperty("left");
+  } else {
+    painelAlvo.classList.add(".hidden");
+    painelAlvo.style.width = "0";
+    painelAlvo.style.height = "0";
+    painelAlvo.style.border = "none";
+
+    if (btnPainel.id === btnPainelEsquerdo?.id) {
+      btnPainel.style.left = "16px";
+    } else if (btnPainel.id == btnPainelDireito?.id) {
+      btnPainel.style.left = "-32px";
+    }
+  }
+
+  definirEstiloGrade(document.body, painelDireito, painelEsquerdo);
+};
+
+let colunasGrade: string[] | null = null;
+// TODO: Ajustar a grade quando a página for redimensionada
+const definirEstiloGrade = (
+  elementoAlvo: HTMLElement | null,
+  painelDireito: HTMLElement | null,
+  painelEsquerdo: HTMLElement | null,
+) => {
+  if (elementoAlvo == null || painelEsquerdo == null || painelDireito == null) return;
+
+  let estiloElementoAlvo = getComputedStyle(elementoAlvo);
+
+  if (colunasGrade == null) {
+    colunasGrade = estiloElementoAlvo.gridTemplateColumns.split(" ");
+  }
+
+  if (painelDireito.classList.contains(".hidden")) {
+    elementoAlvo.style.gridTemplateColumns =
+      colunasGrade[0] + " " + ajustarTamanhoColuna(colunasGrade[1], colunasGrade[2]).join(" ");
+    colunasGrade = elementoAlvo.style.gridTemplateColumns.split(" ");
+  }
+
+  if (painelEsquerdo.classList.contains(".hidden")) {
+    elementoAlvo.style.gridTemplateColumns =
+      ajustarTamanhoColuna(colunasGrade[1], colunasGrade[0]).reverse().join(" ") +
+      " " +
+      colunasGrade[2];
+    colunasGrade = elementoAlvo.style.gridTemplateColumns.split(" ");
+  }
+};
+
+const ajustarTamanhoColuna = (
+  tamanhoColunaCentral: string,
+  tamanhoColunaAdjacente: string,
+): string[] => {
+  if (btnPainelEsquerdo == null) {
+    return ["", ""];
+  }
+
+  let tamanhoCentro: number = converterPXParaNumero(tamanhoColunaCentral);
+  let tamanhoAdjacente: number = converterPXParaNumero(tamanhoColunaAdjacente);
+  let estiloBotao = getComputedStyle(btnPainelEsquerdo);
+  let tamanhoBotao: number = converterPXParaNumero(estiloBotao.width);
+
+  let tamanhoAjustado: number = tamanhoCentro + tamanhoAdjacente - tamanhoBotao;
+  return [tamanhoAjustado + "px", tamanhoBotao + "px"];
 };
 
 // Movimentação dos componentes
