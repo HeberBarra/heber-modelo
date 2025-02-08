c9a51ec4252265afc119ee5ee48ffe1f67c15c25
Heber F Barra <heberferreirabarra@gmail.com>
2025-02-08 18:54:22 -0300

    [ REFACTOR ] Separação do código TS da página de editor em módulos

--- src/main/resources/static/typescript/editor.ts
+++ src/main/resources/static/typescript/editor.ts
@@ -1,3 +1,7 @@
+import { esconderSecoesMenosPrimeira, mudarSecao } from "./editor/mudarSecao.js";
+import { alternarPainel, alternarTextoBotao } from "./editor/alternarPainel.js";
+import { atualizarValorInput, modificarPropriedadeElemento } from "./editor/editorPropriedades.js";
+
 // Trocar aba painel lateral
 let secoesPainelDireito: NodeListOf<HTMLElement> =
   document.querySelectorAll("#painel-direito .pagina");
@@ -16,50 +20,6 @@ let btnProximaSecaoPainelEsquerdo: HTMLButtonElement | null = document.querySele
 let btnVoltarSecaoPainelEsquerdo: HTMLButtonElement | null = document.querySelector(
   "aside#painel-esquerdo button.btn-voltar-secao",
 );
-let indexSecoesPainelDireito: number = 0;
-let indexSecoesPainelEsquerdo: number = 0;
-
-const esconderSecoesExcetoPrimeira = (secoes: NodeListOf<HTMLElement>) => {
-  for (let i: number = 0; i < secoes.length; i++) {
-    if (i == 0) continue;
-
-    secoes[i].style.display = "none";
-  }
-};
-
-const mudarSecao = (secoes: NodeListOf<HTMLElement>, proxima: boolean) => {
-  let indexSecoes =
-    secoes === secoesPainelDireito ? indexSecoesPainelDireito : indexSecoesPainelEsquerdo;
-
-  if (proxima) {
-    indexSecoes++;
-  } else {
-    indexSecoes--;
-  }
-
-  if (indexSecoes < 0) {
-    indexSecoes = secoes.length - 1;
-  }
-
-  if (indexSecoes == secoes.length) {
-    indexSecoes = 0;
-  }
-
-  for (let i: number = 0; i < secoes.length; i++) {
-    if (i == indexSecoes) {
-      secoes[i].style.removeProperty("display");
-      continue;
-    }
-
-    secoes[i].style.display = "none";
-  }
-
-  if (secoes == secoesPainelDireito) {
-    indexSecoesPainelDireito = indexSecoes;
-  } else {
-    indexSecoesPainelEsquerdo = indexSecoes;
-  }
-};
 
 btnProximaSecaoPainelDireito?.addEventListener("click", () => {
   mudarSecao(secoesPainelDireito, true);
@@ -77,11 +37,10 @@ btnVoltarSecaoPainelEsquerdo?.addEventListener("click", () => {
   mudarSecao(secoesPainelEsquerdo, false);
 });
 
-esconderSecoesExcetoPrimeira(secoesPainelDireito);
-esconderSecoesExcetoPrimeira(secoesPainelEsquerdo);
+esconderSecoesMenosPrimeira(secoesPainelDireito);
+esconderSecoesMenosPrimeira(secoesPainelEsquerdo);
 
 // Esconder painel
-const CLASSE_PAINEL_OCULTO: string = "hidden";
 let painelDireito: HTMLElement | null = document.querySelector("#painel-direito");
 let painelEsquerdo: HTMLElement | null = document.querySelector("#painel-esquerdo");
 let btnPainelDireito: HTMLButtonElement | null = document.querySelector(
@@ -107,59 +66,6 @@ btnPainelEsquerdo?.addEventListener("click", (e) => {
 btnPainelEsquerdo?.addEventListener("click", () =>
   alternarPainel(painelEsquerdo, btnPainelEsquerdo),
 );
-
-const alternarTextoBotao = (btnAlvo: HTMLButtonElement | null) => {
-  if (btnAlvo == null) return;
-
-  if (btnAlvo.innerHTML === "&lt;") {
-    btnAlvo.innerHTML = "&gt;";
-  } else {
-    btnAlvo.innerHTML = "&lt;";
-  }
-};
-
-const alternarPainel = (painelAlvo: HTMLElement | null, btnPainel: HTMLButtonElement | null) => {
-  if (painelAlvo == null || btnPainel == null) return;
-
-  if (painelAlvo.classList.contains(CLASSE_PAINEL_OCULTO)) {
-    painelAlvo.classList.remove(CLASSE_PAINEL_OCULTO);
-    painelAlvo.style.removeProperty("border");
-  } else {
-    painelAlvo.classList.add(CLASSE_PAINEL_OCULTO);
-    painelAlvo.style.border = "none";
-  }
-
-  definirEstiloGrade(document.body, painelDireito, painelEsquerdo);
-};
-
-let colunasGrade: string[] | null = null;
-
-const definirEstiloGrade = (
-  elementoAlvo: HTMLElement | null,
-  painelDireito: HTMLElement | null,
-  painelEsquerdo: HTMLElement | null,
-) => {
-  if (elementoAlvo == null || painelEsquerdo == null || painelDireito == null) return;
-
-  let estiloElementoAlvo = getComputedStyle(elementoAlvo);
-
-  if (colunasGrade == null) {
-    colunasGrade = estiloElementoAlvo.gridTemplateColumns.split(" ");
-  }
-
-  if (
-    painelDireito.classList.contains(CLASSE_PAINEL_OCULTO) &&
-    painelEsquerdo.classList.contains(CLASSE_PAINEL_OCULTO)
-  ) {
-    elementoAlvo.style.gridTemplateColumns = "5% 90% 5%";
-  } else if (painelDireito.classList.contains(CLASSE_PAINEL_OCULTO)) {
-    elementoAlvo.style.gridTemplateColumns = "20% 75% 5%";
-  } else if (painelEsquerdo.classList.contains(CLASSE_PAINEL_OCULTO)) {
-    elementoAlvo.style.gridTemplateColumns = "5% 75% 20%";
-  } else {
-    elementoAlvo.style.gridTemplateColumns = "20% 60% 20%";
-  }
-};
 
 // Movimentação dos componentes
 let componentes: NodeListOf<HTMLDivElement> = document.querySelectorAll(".componente");
@@ -223,60 +129,3 @@ editorAltura?.addEventListener("focusout", () => {
 editorLargura?.addEventListener("focusout", () => {
   atualizarValorInput(elementoSelecionado, editorLargura, "width");
 });
-
-const atualizarValorInput = (
-  elemento: HTMLElement | null,
-  elementoInput: HTMLInputElement | null,
-  nomeAtributo: string,
-) => {
-  if (elemento === null || elementoInput === null) return;
-  let valorAtributo = elemento.getAttribute(nomeAtributo);
-
-  if (valorAtributo == null) {
-    throw "Atributo inválido";
-  }
-
-  elementoInput.value = valorAtributo?.substring(0, valorAtributo.length - 2);
-};
-
-const modificarPropriedadeElemento = (
-  elemento: HTMLElement | null,
-  inputValorAtributo: HTMLInputElement | null,
-  nomePropriedade: string,
-) => {
-  if (elemento === null || inputValorAtributo === null) return;
-
-  let novoValor: string = ajustarValorAtributo(inputValorAtributo.value) + "px";
-  elemento.style.setProperty(nomePropriedade, novoValor);
-};
-
-const verificarNumero = (valor: string): boolean => {
-  let regexVerificarNumero: RegExp = /^-?\d+$/g;
-  return regexVerificarNumero.test(valor);
-};
-
-// Verifica uma expressão matemática simples e retorna o resultado, ou caso a expressão seja inválida retorna null.
-// Infelizmente, não é possível utilizar parenteses
-const calcularExpressao = (expressao: string): number | null => {
-  let regexFiltroExpressao: RegExp = /(?<!\S)[0-9]+(?:[-+%~^*\/]+?\d+(?:\.\d+)?)+(?!\S)/g;
-
-  if (expressao.match(regexFiltroExpressao) == null) {
-    return null;
-  }
-
-  return eval(expressao);
-};
-
-const ajustarValorAtributo = (valor: string): number => {
-  if (verificarNumero(valor)) {
-    return parseFloat(valor);
-  }
-
-  let resultadoExpressao = calcularExpressao(valor);
-
-  if (resultadoExpressao == null) {
-    return 0;
-  }
-
-  return resultadoExpressao;
-};
