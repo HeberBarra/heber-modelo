a43496d5b9faf06c5d7dcb93f0586594d12b7334
Heber F Barra <heberferreirabarra@gmail.com>
2025-03-14 09:39:56 -0300

    [ TEST ] Criação de teste para a classe ColetorCodigosSaida

---
+++ src/test/java/io/github/heberbarra/modelador/test/coletor/TestColetorCodigosSaida.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2025 Heber Ferreira Barra, João Gabriel de Cristo, Matheus Jun Alves Matuda.
+ * <p>
+ * Licensed under the Massachusetts Institute of Technology (MIT) License.
+ * You may obtain a copy of the license at:
+ * <p>
+ * https://choosealicense.com/licenses/mit/
+ * <p>
+ * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ */
+package io.github.heberbarra.modelador.test.coletor;
+
+import io.github.heberbarra.modelador.impressor.ColetorCodigosSaida;
+import io.github.heberbarra.modelador.impressor.DadosCodigoSaida;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+public class TestColetorCodigosSaida {
+
+    @Test
+    public void testColetorColetaCodigos() {
+        ColetorCodigosSaida coletorCodigosSaida = new ColetorCodigosSaida();
+        coletorCodigosSaida.coletarCodigosEnum();
+        Set<DadosCodigoSaida> dadosCodigoSaida = coletorCodigosSaida.getDadosCodigosSaida();
+        assert !dadosCodigoSaida.isEmpty();
+    }
+}
