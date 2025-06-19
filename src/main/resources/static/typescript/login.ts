7868e3da776fa5390276216b2c9913b09c1c24e4
Heber F Barra <heberferreirabarra@gmail.com>
2025-06-19 00:10:07 -0300

    [ REFACTOR ] Pequeno ajuste no código para LSP não reclamar de código inválido

--- src/main/resources/static/typescript/login.ts
+++ src/main/resources/static/typescript/login.ts
@@ -14,13 +14,13 @@ let email: HTMLInputElement | null = document.querySelector("input[name='email']
 let senha: HTMLInputElement | null = document.querySelector("input[name='senha']");
 let continuar: HTMLInputElement | null = document.querySelector("#continuar");
 
-const checkInputs = () => {
+function checkInputs(): void {
   if (continuar == null) {
     return;
   }
 
   continuar.disabled = senha?.value.trim().length == 0 && email?.value.trim().length == 0;
-};
+}
 
 email?.addEventListener("input", checkInputs);
 senha?.addEventListener("input", checkInputs);
