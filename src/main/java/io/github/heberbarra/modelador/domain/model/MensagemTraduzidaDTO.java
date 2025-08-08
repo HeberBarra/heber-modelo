579a295dcc18bb4536fac9cf2910a8660588de66
Heber F Barra <heberferreirabarra@gmail.com>
2025-08-08 16:47:58 -0300

    [ FEATURE ] Criação de RestController para pegar a tradução de uma mensagem específica

---
+++ src/main/java/io/github/heberbarra/modelador/domain/model/MensagemTraduzidaDTO.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025. Heber Ferreira Barra, Matheus de Assis de Paula, Matheus Jun Alves Matuda.
+ *
+ * Licensed under the Massachusetts Institute of Technology (MIT) License.
+ * You may obtain a copy of the license at:
+ *
+ *   https://choosealicense.com/licenses/mit/
+ *
+ * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ *
+ */
+
+package io.github.heberbarra.modelador.domain.model;
+
+public class MensagemTraduzidaDTO {
+
+    private String chave;
+    private String mensagem;
+
+    public MensagemTraduzidaDTO(String chave, String mensagem) {
+        this.chave = chave;
+        this.mensagem = mensagem;
+    }
+
+    public String getChave() {
+        return chave;
+    }
+
+    public void setChave(String chave) {
+        this.chave = chave;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
