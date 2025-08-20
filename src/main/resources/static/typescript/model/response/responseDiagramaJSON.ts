ac7b15dc9082fff91c2563554c95610dc16a5f97
Heber F Barra <heberferreirabarra@gmail.com>
2025-08-19 21:14:23 -0300

    [ FEATURE ] Funcionalidade para traduzir os nomes dos diagramas automaticamente na área de seleção de componentes

--- src/main/resources/static/typescript/model/response/responseDiagramaJSON.ts
+++ src/main/resources/static/typescript/model/response/responseDiagramaJSON.ts
@@ -1,21 +1,22 @@
 /**
- * Copyright (C) 2025 Heber Ferreira Barra, João Gabriel de Cristo, Matheus Jun Alves Matuda.
+ * Copyright (c) 2025. Heber Ferreira Barra, Matheus de Assis de Paula, Matheus Jun Alves Matuda.
  *
  * Licensed under the Massachusetts Institute of Technology (MIT) License.
  * You may obtain a copy of the license at:
  *
- *     https://choosealicense.com/licenses/mit/
+ *    https://choosealicense.com/licenses/mit/
  *
  * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
  * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
  */
 
+export interface ResponseDiagramaJSON {
+  nome: string;
+  chaveI18N: string;
+  elementos: TipoComponenteJSON[];
+}
+
 export interface TipoComponenteJSON {
   nome: string;
   tipo: string;
-}
-
-export interface DiagramaJSON {
-  nome: string;
-  elementos: TipoComponenteJSON[];
 }
